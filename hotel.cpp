43ca531c9b2dd646a287f88aa693a8fc0f8dad48
MuhammadHaider12 <haiderarshad660@gmail.com>
2025-11-16 14:44:05 +0500

    second commit

--- hotel.cpp
+++ hotel.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<fstream>
 using namespace std;
-void initializeHotel(int **room,int *roomPerFloor,int &floor ){
+void initializeHotel(int **rooms,int *roomPerFloor,int &floor ){
     cout<<"Enter total number of Floor: ";
     cin>>floor;
     int i,j;
@@ -9,7 +9,7 @@ void initializeHotel(int **room,int *roomPerFloor,int &floor ){
         cout<<"Enter room for floor "<< i << ": ";
         cin>>roomPerFloor[i];
         for(j=0;j<roomPerFloor[i];j++)
-            room[i][j] = 0;
+            rooms[i][j] = 0;
     }
         
 }
@@ -19,14 +19,29 @@ void loadHotelData(int** rooms,  int *roomsPerFloor,int floors, const char* file
     fil>>floors;
     for(i=0;i<floors;i++){
         fil>>roomsPerFloor[i];
-        for(j=0;j<)
+        for(j=0;j<roomsPerFloor[i];j++)
+            fil>>rooms[i][j];
     }
-        
-    
-
-
-
 }
+//  Save room occupancy back to a file before program exit.
+void saveHotelData(int** rooms, int floors, int *roomsPerFloor, const char* filename){
+      ofstream out(filename);
+      out<<floors;
+      out<<"\n";
+      int i,j;
+      for(i=0;i<floors;i++){
+        out<<roomsPerFloor[i]<<endl;
+        for(j=0;j<roomsPerFloor[i];j++)
+            out<<rooms[i][j]<<' ';
+      }
+}
+
+
+void displayHotelStatus(int** rooms, int floors, int roomsPerFloor);
+// → Display all floors and rooms with their occupancy status.
+
+void resetHotel(int** rooms, int floors, int roomsPerFloor);
+// → Mark all rooms as free.
 int main(){
       int **rooms;
       int *roomsPerFloor;
